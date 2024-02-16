4d37a671c0b1da223527bfbbbcfe056fdcf14561
Jepss06 <blackelimit@gmail.com>
2024-02-16 07:51:00 +0700

    publish

---
+++ script/script.js
@@ -0,0 +1,46 @@
+const formatUang = (rupiah) => {
+  return rupiah.toLocaleString("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  });
+};
+//ambil item nama
+
+//ambil item desc
+
+//perintahkan document untuk mengambil id info dan isi variabel untuk menampilkan nama dan desc!
+
+//ambil item total
+
+//ubah element yang id nya total dan tampilkan total !
+
+//buat var saldo
+
+//ubah element yang id nya saldo dan tampilkan saldo!
+
+  //membuat function baru!
+  
+  //mengambil resto-pay dan cash yang berbentuk input type radio, lalu ambil valuenya(true/false)
+
+  // Cek jika tidak memilih metode
+
+  //jika memilih resto
+
+  //jika saldo tidak cukup
+
+  //munculkan konfirmasi dan bungkus atau tampung pakai variabel
+  //jika konfirmasi bersifat true atau ditekan/dipencet "ok"
+
+  //munculkan prompt topup dan bungkus atau tampung menggunakan variabel
+  //jika topup kelipatan 10000
+
+  //tambahkan dengan saldo dan munculkan saldo terbaru
+  //tambahkan keterangan jika kelipatan tidak 10000
+
+  //masuk ke halaman sukses
+  //simpan saldo dalam localstorage
+
+  //jika memilih cash
+
+  //masukan ke halaman cash 
